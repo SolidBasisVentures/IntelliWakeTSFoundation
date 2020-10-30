08ab93e5645d7fb2d01cc8ccc4f5f067c749ace2
Dennis Peters <denjpeters@gmail.com>
2020-10-30 13:40:01 -0400

    Fixed moment objects

--- consoles/consolesMoment.ts
+++ consoles/consolesMoment.ts
@@ -1,12 +1,11 @@
 import moment from 'moment-timezone'
 import {consoleLogTable} from '../src/ConsoleLogTable'
 import {
-	MOMENT_FORMAT_DATE,
-	MOMENT_FORMAT_DATE_TIME,
 	MomentDateString,
 	MomentDateTimeString,
 	MomentDisplayDayDate,
-	MomentDisplayDayDateTime, MomentDisplayTime, MomentTimeString
+	MomentDisplayDayDateTime, MomentDisplayTime,
+	MomentTimeString
 } from '../src/Moment'
 
 export const runMoment = () => {
@@ -16,25 +15,19 @@ export const runMoment = () => {
 		'2020-10-01',
 		'2020-10-01T01:00:00Z',
 		'2020-10-01 01:00:00',
-		// moment()
+		'2019-10-01T01:00:00Z',
+		'2019-10-01 01:00:00',
+		moment()
 	]
 	
 	const fxs: {name: string, function: ((value: any) => any)}[] = [
 		{name: 'moment', function: (value) => moment(value)},
-		// {name: 'moment.format', function: (value) => moment.tz.guess(true)},
-		// {name: 'MomentDateString', function: (value) => MomentDateString(value)},
-		// {name: 'MomentDateString moment', function: (value) => MomentDateString(moment(value))},
-		// {name: 'MomentTimeString', function: (value) => MomentTimeString(value)},
-		{name: 'MomentDateTimeString true', function: (value) => MomentDateTimeString(value, undefined, true)},
-		{name: 'MomentDateTimeString false', function: (value) => MomentDateTimeString(value, undefined, false)},
-		// {name: 'MomentDisplayDayDate', function: (value) => MomentDisplayDayDate(value)},
-		// {name: 'MomentDisplayTime', function: (value) => MomentDisplayTime(value)},
-		{name: 'MomentDisplayDayDateTime false', function: (value) => MomentDisplayDayDateTime(value, false)},
-		{name: 'MomentDisplayDayDateTime true', function: (value) => MomentDisplayDayDateTime(value, true)},
-		// {name: 'MomentDateTimeString moment', function: (value) => MomentDateTimeString(moment(value))},
-		// {name: 'MomentDisplayDayDate moment', function: (value) => MomentDisplayDayDate(moment(value))},
-		// {name: 'MomentDisplayTime moment', function: (value) => MomentDisplayTime(moment(value))},
-		// {name: 'MomentDisplayDayDateTime moment', function: (value) => MomentDisplayDayDateTime(moment(value))}
+		{name: 'MomentDateTimeString', function: (value) => MomentDateTimeString(value)},
+		{name: 'MomentDateString', function: (value) => MomentDateString(value)},
+		{name: 'MomentTimeString', function: (value) => MomentTimeString(value)},
+		{name: 'MomentDisplayDayDateTime', function: (value) => MomentDisplayDayDateTime(value)},
+		{name: 'MomentDisplayDayDate', function: (value) => MomentDisplayDayDate(value)},
+		{name: 'MomentDisplayTime', function: (value) => MomentDisplayTime(value)}
 	]
 	
 	let results: any[][] = [
@@ -62,18 +55,4 @@ export const runMoment = () => {
 	}
 	
 	consoleLogTable(results)
-	
-	//Servers run on UTC
-	// moment.tz.setDefault("UTC")
-	//
-	// console.log(new Date())
-	// console.log(moment().tz('America/New_York'))
-	// console.log(moment.utc())
-	//
-	// //Simulate browser EST
-	// moment.tz.setDefault("America/New_York")
-	//
-	// console.log(new Date())
-	// console.log(moment())
-	// console.log(moment.utc())
 }
--- src/Moment.ts
+++ src/Moment.ts
@@ -7,385 +7,87 @@ export const MOMENT_FORMAT_DATE_TIME = MOMENT_FORMAT_DATE + ' ' + MOMENT_FORMAT_
 
 const DATE_FORMAT_TRIES: moment.MomentFormatSpecification = ['YYYY-MM-DD', 'M-D-YYYY', 'MM-DD-YYYY', moment.ISO_8601]
 const TIME_FORMAT_TRIES: moment.MomentFormatSpecification = [
-	'HH:mm:ss',
-	'HH:mm',
+	moment.ISO_8601,
 	'YYYY-MM-DD HH:mm:ss',
 	'YYYY-MM-DD HH:mm',
+	'HH:mm:ss',
+	'HH:mm',
 	'D-M-YYYY HH:mm:ss',
 	'D-M-YYYY HH:mm',
 	'DD-MM-YYYY HH:mm:ss',
-	'DD-MM-YYYY HH:mm',
-	moment.ISO_8601
+	'DD-MM-YYYY HH:mm'
 ]
 
-export const MomentFromUTCString = (date: string | Moment | Date | null | undefined): string | null => {
-	if (!date) {
-		return null
-	}
-	
-	const momentString = MomentDateTimeString(date, undefined, true)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	const workingMoment = moment.utc(momentString).toISOString()
-	
-	return MomentDateTimeString(workingMoment, MOMENT_FORMAT_DATE_TIME, true)
-}
-
-export const MomentWithFormatUTC = (
-	value: string | Moment | Date | null | undefined,
-	format: moment.MomentFormatSpecification | undefined,
-	strict: boolean,
-	convertFromUTC: boolean
-): Moment => {
-	return convertFromUTC ? moment(value, format, strict) : moment.utc(value, format, strict)
-}
-
-export const MomentTimeString = (
-	value: string | Moment | Date | null | undefined,
-	format = MOMENT_FORMAT_TIME_SECONDS,
-	convertFromUTC = false
-): string | null => {
-	if (!value) {
-		return null
-	}
-	
-	if (typeof value !== 'string') {
-		const momentObject = MomentWithFormatUTC(value, undefined, false, convertFromUTC)
-		if (momentObject.isValid()) {
-			return momentObject.format(format)
-		}
-		return null
-	}
-	
-	const valueTry = value.replace('T', ' ').substr(0, 19)
-	
-	for (const formatTry of TIME_FORMAT_TRIES) {
-		const momentObject = MomentWithFormatUTC(valueTry, formatTry, true, convertFromUTC)
-		if (momentObject.isValid()) {
-			return momentObject.format(format)
-		}
-	}
-	
-	return null
-}
-
-export const MomentDateString = (
-	value: string | Moment | Date | null | undefined,
-	format = MOMENT_FORMAT_DATE,
-	convertFromUTC = false
-): string | null => {
-	if (!value) {
-		return null
-	}
-	
-	if (typeof value !== 'string') {
-		const momentObject = MomentWithFormatUTC(value, undefined, false, convertFromUTC)
-		if (momentObject.isValid()) {
-			return momentObject.format(format)
-		}
-		return null
-	}
-	
-	const valueTry = value.replace('T', ' ').substr(0, 10)
-	
-		const momentObject = MomentWithFormatUTC(valueTry, DATE_FORMAT_TRIES, true, convertFromUTC)
-		if (momentObject.isValid() && momentObject.year() >= 1900) {
-			return momentObject.format(format)
-		}
-	
-	return null
-}
-
-export const MomentDateTimeString = (
-	value: string | Moment | Date | null | undefined,
-	format = MOMENT_FORMAT_DATE_TIME,
-	convertFromUTC = false
-): string | null => {
-	const momentDate = MomentDateString(value, undefined, convertFromUTC)
-	
-	if (!!momentDate) {
-		const momentTime = MomentTimeString(value, undefined, convertFromUTC)
-		
-		if (!!momentTime) {
-			return moment(momentDate + ' ' + momentTime).format(format)
-		}
-	}
-	
-	return null
-}
-
-export const MomentDisplayDayDateTime = (
-	date: string | Moment | Date | null | undefined,
-	convertFromUTC: boolean
-): string | null => {
-	if (!date) {
-		return null
-	}
-	
-	const momentString = MomentDateTimeString(date, MOMENT_FORMAT_DATE_TIME, false)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	// const workingMoment = convertFromUTC ? moment.utc(date) : moment(date)
-	const workingMoment = moment.utc(date)
-	
-	const format = workingMoment.year() === moment().year() ? 'ddd, MMM D, h:mm a' : 'ddd, MMM D, YYYY @ h:mm a'
-	
-	if (convertFromUTC) {
-		return workingMoment.local().format(format)
-	} else {
-		return workingMoment.format(format)
-	}
-}
-
-export const MomentDisplayDayDate = (
-	date: string | Moment | Date | null | undefined,
-	convertFromUTC = true
-): string | null => {
-	if (!date) {
-		return null
-	}
-	
-	const momentString = MomentDateString(date, MOMENT_FORMAT_DATE_TIME, false)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	// const workingMoment = convertFromUTC ? moment.utc(date) : moment(date)
-	const workingMoment = moment.utc(date)
-	
-	const format = workingMoment.year() === moment().year() ? 'ddd, MMM D' : 'ddd, MMM D, YYYY'
-	
-	if (convertFromUTC) {
-		return workingMoment.local().format(format)
-	} else {
-		return workingMoment.format(format)
-	}
-}
-
-export const MomentDisplayTime = (
-	date: string | Moment | Date | null | undefined,
-	convertFromUTC = true
-): string | null => {
-	if (!date) {
-		return null
-	}
-	
-	const momentString = MomentTimeString(date, MOMENT_FORMAT_TIME_SECONDS, false)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	// const workingMoment = convertFromUTC
-	// 	? moment(moment().format(MOMENT_FORMAT_DATE) + ' ' + momentString)
-	// 	: moment.utc(moment().format(MOMENT_FORMAT_DATE) + ' ' + momentString)
-	const workingMoment = moment.utc(moment().format(MOMENT_FORMAT_DATE) + ' ' + momentString)
-	
-	const format = 'h:mm a'
-	
-	if (convertFromUTC) {
-		return workingMoment.local().format(format)
-	} else {
-		return workingMoment.format(format)
-	}
-}
-
-/* export const MOMENT_FORMAT_DATE = 'YYYY-MM-DD'
-export const MOMENT_FORMAT_TIME_SECONDS = 'HH:mm:ss'
-export const MOMENT_FORMAT_TIME_NO_SECONDS = 'HH:mm'
-export const MOMENT_FORMAT_DATE_TIME = MOMENT_FORMAT_DATE + ' ' + MOMENT_FORMAT_TIME_SECONDS
-
-// export const MomentFromUTCString = (date: string | Moment | Date | null | undefined): string | null => {
-// 	if (!date) {
-// 		return null
-// 	}
-//
-// 	const momentString = MomentDateTimeString(date, undefined, true)
-//
-// 	if (!momentString) {
-// 		return null
-// 	}
-//
-// 	const workingMoment = moment.utc(momentString).toISOString()
-//
-// 	return MomentDateTimeString(workingMoment, MOMENT_FORMAT_DATE_TIME, true)
-// }
+const StringHasTimeZoneData = (value: string): boolean => value.includes('T')
 
 export const MomentCurrentTimeZone = (): string => (moment().tz() ?? 'UTC').toString()
 
-export const MomentWithFormatUTC = (
-	value: string | Moment | Date | null | undefined,
-	format: string | MomentBuiltinFormat | undefined,
-	strict: boolean,
-	convertFromUTC: boolean
-): Moment => {
-	return convertFromUTC ? moment(value, format, strict) : moment.utc(value, format, strict)
-}
-
-export const MomentDateString = (
-	value: string | Moment | Date | null | undefined,
-	format = MOMENT_FORMAT_DATE
-): string | null => {
+export const MomentFromString = (
+	value: string | Moment | Date | null | undefined
+): Moment | null => {
 	if (!value) {
 		return null
 	}
 	
 	if (typeof value !== 'string') {
-		const momentObject = moment.utc(value)
+		const momentObject = moment(value)
 		if (momentObject.isValid()) {
-			return momentObject.tz(MomentCurrentTimeZone()).format(format)
+			return momentObject.utc().tz(MomentCurrentTimeZone())
 		}
-		return null
-	}
-	
-	const FORMAT_TRIES = ['YYYY-MM-DD', 'M-D-YYYY', 'MM-DD-YYYY', moment.ISO_8601]
-	
-	const valueTry = value.replace('T', ' ').substr(0, 10)
-	
-	const momentObject = moment(valueTry, FORMAT_TRIES, true).utc()
-	if (momentObject.isValid() && momentObject.year() >= 1900) {
-		return momentObject.tz(MomentCurrentTimeZone()).format(format)
+	} else {
+		const momentObject = StringHasTimeZoneData(value) ? moment(value, [...DATE_FORMAT_TRIES, ...TIME_FORMAT_TRIES], true) : moment.utc(value, [...DATE_FORMAT_TRIES, ...TIME_FORMAT_TRIES], true)
+		if (momentObject.isValid()) {
+			return momentObject
+		}
 	}
 	
 	return null
 }
 
 export const MomentTimeString = (
-	value: string | Moment | Date | null | undefined,
-	format = MOMENT_FORMAT_TIME_SECONDS
+	value: string | Moment | Date | null | undefined
 ): string | null => {
-	if (!value) {
-		return null
-	}
-	
-	if (typeof value !== 'string') {
-		const momentObject = moment(value, undefined, false)
-		if (momentObject.isValid()) {
-			return momentObject.format(format)
-		}
-		return null
-	}
-	
-	const FORMAT_TRIES = [
-		'HH:mm:ss',
-		'HH:mm',
-		'YYYY-MM-DD HH:mm:ss',
-		'YYYY-MM-DD HH:mm',
-		'D-M-YYYY HH:mm:ss',
-		'D-M-YYYY HH:mm',
-		'DD-MM-YYYY HH:mm:ss',
-		'DD-MM-YYYY HH:mm',
-		moment.ISO_8601
-	]
-	
-	const valueTry = value.replace('T', ' ').substr(0, 19)
-	
-	for (const formatTry of FORMAT_TRIES) {
-		const momentObject = moment(valueTry, formatTry, true)
-		if (momentObject.isValid()) {
-			return momentObject.format(format)
-		}
-	}
-	
-	return null
+	return MomentFromString(value)?.format(MOMENT_FORMAT_TIME_SECONDS) ?? null
 }
+
+export const MomentDateString = (
+	value: string | Moment | Date | null | undefined
+): string | null => {
+	return MomentFromString(value)?.format(MOMENT_FORMAT_DATE) ?? null
+}
+
 export const MomentDateTimeString = (
-	value: string | Moment | Date | null | undefined,
-	format = MOMENT_FORMAT_DATE_TIME
+	value: string | Moment | Date | null | undefined
 ): string | null => {
-	const momentDate = MomentDateString(value, undefined)
-	
-	if (!!momentDate) {
-		const momentTime = MomentTimeString(value, undefined)
-		
-		if (!!momentTime) {
-			return moment(momentDate + ' ' + momentTime).format(format)
-		}
-	}
-	
-	return null
+	return MomentFromString(value)?.format(MOMENT_FORMAT_DATE_TIME) ?? null
 }
+
 export const MomentDisplayDayDateTime = (
-	date: string | Moment | Date | null | undefined,
-	convertFromUTC = true
+	value: string | Moment | Date | null | undefined
 ): string | null => {
-	if (!date) {
+	const momentObject = MomentFromString(value)
+	
+	if (!momentObject) {
 		return null
 	}
 	
-	const momentString = MomentDateTimeString(date, MOMENT_FORMAT_DATE_TIME)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	// const workingMoment = convertFromUTC ? moment.utc(date) : moment(date)
-	const workingMoment = moment.utc(date)
-	
-	const format = workingMoment.year() === moment().year() ? 'ddd, MMM D, h:mm a' : 'ddd, MMM D, YYYY @ h:mm a'
-	
-	if (convertFromUTC) {
-		return workingMoment.local().format(format)
-	} else {
-		return workingMoment.format(format)
-	}
+	return momentObject.format(momentObject.year() === moment().year() ? 'ddd, MMM D, h:mm a' : 'ddd, MMM D, YYYY @ h:mm a')
 }
+
 export const MomentDisplayDayDate = (
-	date: string | Moment | Date | null | undefined,
-	convertFromUTC = true
+	value: string | Moment | Date | null | undefined
 ): string | null => {
-	if (!date) {
+	const momentObject = MomentFromString(value)
+	
+	if (!momentObject) {
 		return null
 	}
 	
-	const momentString = MomentDateString(date, MOMENT_FORMAT_DATE_TIME)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	// const workingMoment = convertFromUTC ? moment.utc(date) : moment(date)
-	const workingMoment = moment.utc(date)
-	
-	const format = workingMoment.year() === moment().year() ? 'ddd, MMM D' : 'ddd, MMM D, YYYY'
-	
-	if (convertFromUTC) {
-		return workingMoment.local().format(format)
-	} else {
-		return workingMoment.format(format)
-	}
+	return momentObject.format(momentObject.year() === moment().year() ? 'ddd, MMM D' : 'ddd, MMM D, YYYY')
 }
+
 export const MomentDisplayTime = (
-	date: string | Moment | Date | null | undefined,
-	convertFromUTC = true
+	value: string | Moment | Date | null | undefined
 ): string | null => {
-	if (!date) {
-		return null
-	}
-	
-	const momentString = MomentTimeString(date, MOMENT_FORMAT_TIME_SECONDS)
-	
-	if (!momentString) {
-		return null
-	}
-	
-	// const workingMoment = convertFromUTC
-	// 	? moment(moment().format(MOMENT_FORMAT_DATE) + ' ' + momentString)
-	// 	: moment.utc(moment().format(MOMENT_FORMAT_DATE) + ' ' + momentString)
-	const workingMoment = moment.utc(moment().format(MOMENT_FORMAT_DATE) + ' ' + momentString)
-	
-	const format = 'h:mm a'
-	
-	if (convertFromUTC) {
-		return workingMoment.local().format(format)
-	} else {
-		return workingMoment.format(format)
-	}
-} */
+	return MomentFromString(value)?.format('h:mm a') ?? null
+}
