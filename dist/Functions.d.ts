7fa9b2cdd79416b774ef08e2c696afbed57dfcc6
Dennis Peters <denjpeters@gmail.com>
2020-10-31 09:34:48 -0400

    Added Address structure

--- dist/Functions.d.ts
+++ dist/Functions.d.ts
@@ -5,6 +5,13 @@ export declare const IsValidInputDecimal: (value: string) => boolean;
 export declare const GenerateUUID: () => string;
 export declare const IsOn: (value: any) => boolean;
 export declare type TFindIsActive = boolean | null;
+export interface IAddress {
+    address_1: string;
+    address_2: string;
+    city: string;
+    state: string;
+    zip: string;
+}
 export declare const AddressCopy: (fromObject: any, fromPrefix: string, toObject: any, toPrefix: string, includeName?: boolean, includePhone?: boolean, includeTimeZone?: boolean, includeGPS?: boolean) => void;
 export declare const AddressValid: (address: any, prefix?: string | undefined) => boolean;
 export declare const AddressSingleRow: (object: any, prefix?: string | undefined) => string;
