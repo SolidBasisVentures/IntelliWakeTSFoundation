bff99b13e0ac45fc2f2009d1d7b0e893bf393633
Dennis Peters <denjpeters@gmail.com>
2021-06-11 21:21:07 -0400

    DeepEqual

--- src/Functions.test.ts
+++ src/Functions.test.ts
@@ -1,41 +1,79 @@
-import {AddS, IsOn, RoundTo} from './Functions'
+import {AddS, DeepEqual, IsOn, RoundTo} from './Functions'
 
 test('IsOn 1', () => {
-	expect(IsOn(1)).toBe(true);
+	expect(IsOn(1)).toBe(true)
 })
 
 test('IsOn null', () => {
-	expect(IsOn(null)).toBe(false);
+	expect(IsOn(null)).toBe(false)
 })
 
 test('IsOn active', () => {
-	expect(IsOn('active')).toBe(true);
+	expect(IsOn('active')).toBe(true)
 })
 
-test ('RoundTo 10.15, 1', () => {
+test('RoundTo 10.15, 1', () => {
 	expect(RoundTo(10.15, 1)).toBe(10.2)
 })
 
-test ('RoundTo 10.14, 1', () => {
+test('RoundTo 10.14, 1', () => {
 	expect(RoundTo(10.14, 1)).toBe(10.1)
 })
 
-test ('RoundTo 10.15, 0', () => {
+test('RoundTo 10.15, 0', () => {
 	expect(RoundTo(10.15, 0)).toBe(10)
 })
 
-test ('RoundTo 10.5, 0', () => {
+test('RoundTo 10.5, 0', () => {
 	expect(RoundTo(10.5, 0)).toBe(11)
 })
 
-test ('AddS 0 True', () => {
+test('AddS 0 True', () => {
 	expect(AddS('Row', 0)).toBe('Rows')
 })
 
-test ('AddS 1 False', () => {
+test('AddS 1 False', () => {
 	expect(AddS('Row', 1)).toBe('Row')
 })
 
-test ('AddS 2 True', () => {
+test('AddS 2 True', () => {
 	expect(AddS('Row', 2)).toBe('Rows')
+})
+
+const item = {
+	val1: true,
+	val2: 1,
+	val3: 'One',
+	val4: {
+		valA: 1,
+		valB: 'Two'
+	},
+	val5: [
+		'One',
+		'Two'
+	],
+	val6: [
+		{item1: 1, item2: 'One'},
+		{item1: 2, item2: 'Two'}
+	]
+}
+
+test('Deep Equal', () => {
+	expect(DeepEqual(item, {...item})).toBe(true)
+})
+
+test('Deep Equal - Fail Property', () => {
+	expect(DeepEqual(item, {...item, val1: false})).toBe(false)
+})
+
+test('Deep Equal - Fail Object', () => {
+	expect(DeepEqual(item, {...item, val4: {...item.val4, valA: 2}})).toBe(false)
+})
+
+test('Deep Equal - Fail Array', () => {
+	expect(DeepEqual(item, {...item, val5: ['Two', 'One']})).toBe(false)
+})
+
+test('Deep Equal - Fail Array Size', () => {
+	expect(DeepEqual(item, {...item, val5: ['One']})).toBe(false)
 })
--- src/Functions.ts
+++ src/Functions.ts
@@ -388,3 +388,55 @@ export const filterAsync = async <T>(
 	
 	return returnArray
 }
+
+export const DeepEqual = (object1: any, object2: any): boolean => {
+	if ((!object1 && !!object2) || (!!object1 && !object2) || typeof object1 !== typeof object2) return false
+	
+	if (Array.isArray(object1)) {
+		if (object1.length !== object2.length) return false
+		
+		for (let i = 0; i < object1.length; i++) {
+			if (!DeepEqual(object1[i], object2[i])) return false
+		}
+		
+		return true
+	}
+	
+	switch (typeof object1) {
+		case 'function':
+			return true
+		case 'object':
+			if (typeof object1 === 'object' && (object1 as any).type?.toString().includes('react.')) return true
+			if (typeof object2 === 'object' && (object2 as any).type?.toString().includes('react.')) return true
+			
+			const keys1 = Object.keys(object1)
+			const keys2 = Object.keys(object2)
+			
+			if (keys1.length !== keys2.length) {
+				return false
+			}
+			
+			for (const key of keys1) {
+				const val1 = object1[key]
+				const val2 = object2[key]
+				
+				if (typeof val1 !== typeof val2) return false
+				
+				const areObjects = isObject(val1) && isObject(val2)
+				if (
+					(areObjects && !DeepEqual(val1, val2)) ||
+					(!areObjects && val1 !== val2)
+				) {
+					return false
+				}
+			}
+			
+			return true
+		default:
+			return object1 === object2
+	}
+}
+
+function isObject(object: any) {
+	return object !== null && object !== undefined && typeof object === 'object'
+}
