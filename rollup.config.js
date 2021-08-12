9512bb8799762c25386736aad04d629cb1f38b52
Dennis Peters <denjpeters@gmail.com>
2021-08-12 13:45:58 -0400

    Dayjs required by children

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,18 @@
 import pkg from './package.json'
 import typescript from 'rollup-plugin-typescript2'
-import nodeResolve from '@rollup/plugin-node-resolve'
 
 export default {
-    input: [
-        'src/main.ts'
-    ],
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs'
-	        // file: 'dist/main.js',
-	        // format: 'esm'
-        }
-    ],
-    plugins: [typescript({ objectHashIgnoreUnknownHack: false }), nodeResolve()],
-    external: []
+	input: [
+		'src/main.ts'
+	],
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs'
+			// file: 'dist/main.js',
+			// format: 'esm'
+		}
+	],
+	plugins: [typescript({objectHashIgnoreUnknownHack: false})],
+	external: ['dayjs', 'dayjs/plugin/duration', 'dayjs/plugin/isoWeek', 'dayjs/plugin/utc', 'dayjs/plugin/timezone', 'dayjs/plugin/AdvancedFormat', 'dayjs/plugin/LocalizedFormat', 'dayjs/plugin/customParseFormat']
 }
