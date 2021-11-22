5328859adbb3eb0c015edb68bf88305384964b3d
Dennis Peters <denjpeters@gmail.com>
2021-11-22 13:36:44 -0500

    Updated

--- dist/DateManager.d.ts
+++ dist/DateManager.d.ts
@@ -25,22 +25,22 @@ export declare const StringHasTimeData: (value: string) => boolean;
 export declare const StringHasDateData: (value: string) => boolean;
 export declare const StringHasTimeZoneData: (value: string) => boolean;
 export declare const IsDateString: (value: any) => boolean;
-export declare type TDateAny = Date | number | string | null | undefined;
+export declare type TDateAny = Date | number | 'now' | 'today' | string | null | undefined;
 export declare type TDateParseOptions = TAdjustment & {
     timezoneSource?: string;
 };
-export declare const DateParseTS: (date?: TDateAny, adjustements?: TDateParseOptions | undefined) => number | null;
-export declare const DateISO: (date?: TDateAny, adjustements?: TDateParseOptions | undefined) => string | null;
-export declare const DateObject: (date?: TDateAny, adjustements?: TDateParseOptions | undefined) => Date | null;
-export declare const DateICS: (date?: TDateAny, adjustements?: TDateParseOptions | undefined) => string | null;
+export declare const DateParseTS: (date: TDateAny, adjustements?: TDateParseOptions | undefined) => number | null;
+export declare const DateISO: (date: TDateAny, adjustements?: TDateParseOptions | undefined) => string | null;
+export declare const DateObject: (date: TDateAny, adjustements?: TDateParseOptions | undefined) => Date | null;
+export declare const DateICS: (date: TDateAny, adjustements?: TDateParseOptions | undefined) => string | null;
 export declare type TDateFormat = 'Local' | 'Date' | 'DisplayDate' | 'DisplayTime' | 'DisplayDateDoW' | 'DisplayDateTime' | 'DisplayDateDoWTime' | 'DisplayDateLong' | 'DisplayDateDoWLong' | 'DisplayDateTimeLong' | 'DisplayDateDoWTimeLong';
-export declare const DateFormatAny: (format: TDateFormat | string, date?: TDateAny, timezoneDisplay?: string | undefined, timezoneSource?: string | undefined) => string | null;
-export declare const DateFormat: (format: TDateFormat, date?: TDateAny, timezoneDisplay?: string | undefined, timezoneSource?: string | undefined) => string | null;
-export declare const YYYYMMDDHHmmss: (date?: TDateAny) => string;
-export declare const YYYY_MM_DD_HH_mm_ss: (date?: TDateAny) => string;
-export declare const YYYYsMMsDDsHHcmmcss: (date?: TDateAny) => string;
-export declare const YYYYsMMsDD: (date?: TDateAny) => string;
-export declare const HHcmmcss: (date?: TDateAny) => string;
+export declare const DateFormatAny: (format: TDateFormat | string, date: TDateAny, timezoneDisplay?: string | undefined, timezoneSource?: string | undefined) => string | null;
+export declare const DateFormat: (format: TDateFormat, date: TDateAny, timezoneDisplay?: string | undefined, timezoneSource?: string | undefined) => string | null;
+export declare const YYYYMMDDHHmmss: (date: TDateAny) => string;
+export declare const YYYY_MM_DD_HH_mm_ss: (date: TDateAny) => string;
+export declare const YYYYsMMsDDsHHcmmcss: (date: TDateAny) => string;
+export declare const YYYYsMMsDD: (date: TDateAny) => string;
+export declare const HHcmmcss: (date: TDateAny) => string;
 export declare const MonthNames: string[];
 export declare const WeekDays: string[];
 export declare const TSYearsEstimate: (ts: number) => number;
@@ -52,7 +52,7 @@ export declare const TSMinutes: (ts: number, withinHour?: boolean | undefined) =
 export declare const TSSeconds: (ts: number, withinMinute?: boolean | undefined) => number;
 export declare const DateAdjustTS: (date: TDateAny, adjustments: TAdjustment) => number | null;
 export declare const DateDiff: (dateFrom: TDateAny, dateTo: TDateAny, duration: TDuration) => number | null;
-export declare const DateDiffComponents: (dateFrom: TDateAny, dateTo?: TDateAny) => {
+export declare const DateDiffComponents: (dateFrom: TDateAny, dateTo: TDateAny) => {
     year: number;
     month: number;
     day: number;
@@ -61,12 +61,12 @@ export declare const DateDiffComponents: (dateFrom: TDateAny, dateTo?: TDateAny)
     second: number;
     millisecond: number;
 };
-export declare const DateDiffLongDescription: (dateFrom: TDateAny, dateTo?: TDateAny, trimSeconds?: boolean) => string;
+export declare const DateDiffLongDescription: (dateFrom: TDateAny, dateTo: TDateAny, trimSeconds?: boolean, abbreviated?: boolean) => string;
 /**
  * Displays a simplified duration format from seconds.
  *
  * @example
  * MomentDurationShortText((30 * 60) + 20) // result: 30 Minutes 20 Seconds
  */
-export declare const DurationLongDescription: (seconds: number, trimSeconds?: boolean) => string;
-export declare const DateCompare: (date1: TDateAny, evalType: 'IsSame' | 'IsBefore' | 'IsAfter' | 'IsSameOrBefore' | 'IsSameOrAfter', date2?: TDateAny, atInterval?: "year" | "years" | "month" | "months" | "week" | "weeks" | "day" | "days" | "hour" | "hours" | "minute" | "minutes" | "second" | "seconds" | "millisecond" | "milliseconds" | undefined) => boolean;
+export declare const DurationLongDescription: (seconds: number, trimSeconds?: boolean, abbreviated?: boolean) => string;
+export declare const DateCompare: (date1: TDateAny, evalType: 'IsSame' | 'IsBefore' | 'IsAfter' | 'IsSameOrBefore' | 'IsSameOrAfter', date2: TDateAny, minInterval?: "year" | "years" | "month" | "months" | "week" | "weeks" | "day" | "days" | "hour" | "hours" | "minute" | "minutes" | "second" | "seconds" | "millisecond" | "milliseconds" | undefined) => boolean;
