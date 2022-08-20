c83f9ec3fdd1fd281dff47314217c816312eea8f
Dennis Peters <denjpeters@gmail.com>
2022-08-20 16:15:13 -0400

    Nullable

--- dist/DeepEqual.d.ts
+++ dist/DeepEqual.d.ts
@@ -1,1 +1,7 @@
 export declare const DeepEqual: (object1: any, object2: any) => boolean;
+export declare type Nullable<T> = {
+    [K in keyof T]: T[K] | null;
+};
+export declare type DeepNullable<T> = {
+    [K in keyof T]: DeepNullable<T[K]> | null;
+};
--- src/DeepEqual.ts
+++ src/DeepEqual.ts
@@ -66,3 +66,9 @@ export const DeepEqual = (object1: any, object2: any): boolean => {
 			return object1 === object2
 	}
 }
+
+export type Nullable<T> = { [K in keyof T]: T[K] | null }
+
+export type DeepNullable<T> = {
+	[K in keyof T]: DeepNullable<T[K]> | null
+}
