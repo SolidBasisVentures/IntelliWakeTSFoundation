d539bc7842769f10d70993117d549c6a035197d7
Dennis Peters <denjpeters@gmail.com>
2022-10-27 21:40:34 -0400

    Enum Functions

--- src/Enums.ts
+++ src/Enums.ts
@@ -2,6 +2,6 @@ export const EnumValues = <T extends object>(enumumerator: T): T[] => Object.val
 
 export const EnumKeys = <T extends object>(enumerator: T) => Object.keys(enumerator)
 
-export const EnumKeyFromValue = <T extends object>(enumumerator: T, value: T | string): string | undefined => Object.keys(enumumerator)[Object.values(enumumerator).indexOf(value as any)] as unknown as string
+export const EnumKeyFromValue = <T extends object>(enumumerator: T, value: T | string | null | undefined): string | undefined => !value ? undefined : Object.keys(enumumerator)[Object.values(enumumerator).indexOf(value as any)] as unknown as string
 
-export const EnumValueFromKey = <T extends object>(enumumerator: T, value: string): T | undefined => Object.values(enumumerator)[Object.keys(enumumerator).indexOf(value as any)] as unknown as T
+export const EnumValueFromKey = <T extends object>(enumumerator: T, key: string | null | undefined): T | undefined => !key ? undefined : Object.values(enumumerator)[Object.keys(enumumerator).indexOf(key as any)] as unknown as T
