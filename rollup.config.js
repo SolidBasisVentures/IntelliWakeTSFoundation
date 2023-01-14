5fb41ee47f3d00c61428c12b93ebfdcaed6063c3
Dennis Peters <denjpeters@gmail.com>
2023-01-14 12:07:16 -0500

    Update configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,17 @@ import pkg from './package.json'
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-	input: [
-		'src/main.ts'
-	],
-	output: [
-		{
-			file: pkg.main,
-			format: 'esm'
-			// format: 'cjs'
-			// file: 'dist/main.js',
-		}
-	],
-	plugins: [typescript({objectHashIgnoreUnknownHack: false})],
-	external: ['moment-timezone']
+    input: [
+        'src/main.ts'
+    ],
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs'
+	        // file: 'dist/main.js',
+	        // format: 'esm'
+        }
+    ],
+    plugins: [typescript({ objectHashIgnoreUnknownHack: false })],
+    external: ['moment-timezone']
 }
