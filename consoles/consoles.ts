0e528639e85072bad84c176569f66d7cf849f747
Dennis Peters <denjpeters@gmail.com>
2023-04-27 12:56:43 -0400

    Added a TimeTracker class

--- consoles/consoles.ts
+++ consoles/consoles.ts
@@ -1,10 +1,35 @@
-import {DateISO, DateOnly, ESTTodayDateTimeLabel, TimeOnly} from '../src/DateManager'
+import {ESTTodayDateTimeLabel} from '../src/DateManager'
+import {TimeTracker} from '../src/TimeTracker'
+import {Sleep} from '../src/Functions'
 
 require('source-map-support').install()
 
 console.log('Starting', ESTTodayDateTimeLabel())
 console.time('Consoles')
 
-console.log(DateOnly('2023-03-08', {week: 'StartOf', timezoneDisplay: 'America/New_York'}))
+const timeTracker = new TimeTracker({offendingMS: 350, warnAutomatically: false})
 
-console.timeEnd('Consoles')
+Sleep(300)
+	.then(() => timeTracker.mark('First Event'))
+	.then(() => Sleep(400))
+	.then(() => timeTracker.mark('Second Event'))
+	.then(() => Sleep(600))
+	.then(() => timeTracker.mark('Third Event'))
+	.then(() =>
+		timeTracker.markResolved(
+			'Sleep Two-Fifty',
+			Sleep(250).then(() => 'Sleep 250')
+		)
+	)
+	.then((res) => console.log('Resolved', res))
+	.then(() =>
+		timeTracker.markResolved(
+			'Sleep Four Hundred',
+			Sleep(400).then(() => 'Sleep 400')
+		)
+	)
+	.then((res) => console.log('Resolved', res))
+	.then(() =>
+		timeTracker.durationOffends({label: 'Test Event', offendingMS: 1000, warnAutomatically: true, showAll: true})
+	)
+	.then(() => console.timeEnd('Consoles'))
---
+++ src/TimeTracker.ts
@@ -0,0 +1,139 @@
+import {GreaterNumber, LeastNumber, OmitProperty} from './Functions'
+import {ESTTodayDateTimeLabel} from './DateManager'
+import {ToDigits} from './StringManipulation'
+
+export type TTimeEvent = {
+	eventMS: number
+	label: string
+	durationMS: number
+}
+
+export type TTimeTrackerOptions = {
+	offendingMS?: number | null
+	warnAutomatically?: boolean | null
+}
+
+export class TimeTracker {
+	public events: TTimeEvent[]
+	public offendingMS: number
+	public warnAutomatically = true
+
+	constructor(options?: TTimeTrackerOptions) {
+		this.events = [
+			{
+				eventMS: new Date().valueOf(),
+				label: 'Start',
+				durationMS: 0
+			}
+		]
+
+		this.offendingMS = options?.offendingMS ?? 500
+		this.warnAutomatically = options?.warnAutomatically ?? true
+	}
+
+	/**
+	 * Reset the control to start tracking times from this point forward
+	 */
+	public reset() {
+		this.events = [
+			{
+				eventMS: new Date().valueOf(),
+				label: 'Start',
+				durationMS: 0
+			}
+		]
+	}
+
+	/**
+	 * Get the maximum TS in the events
+	 */
+	public get maxTS() {
+		return this.events.reduce<number>((result, item) => GreaterNumber(result, item.eventMS), 0)
+	}
+
+	/**
+	 * Get the minimum TS in the events
+	 */
+	public get minTS() {
+		return this.events.reduce<number>((result, item) => LeastNumber(result, item.eventMS), this.maxTS)
+	}
+
+	/**
+	 * Get the total duration of all events
+	 */
+	public get duration() {
+		return this.maxTS - this.minTS
+	}
+
+	/**
+	 * Mark the current time with a label for analysis later, and throw a console warn if appropriate
+	 * @param label
+	 * @param options
+	 */
+	public mark(label: string, options?: TTimeTrackerOptions) {
+		const maxTS = this.maxTS
+		const start = new Date().valueOf()
+		const event: TTimeEvent = {
+			eventMS: start,
+			label,
+			durationMS: start - maxTS
+		}
+		this.events.push(event)
+
+		if (
+			(options?.warnAutomatically ?? this.warnAutomatically) &&
+			event.durationMS > (options?.offendingMS ?? this.offendingMS)
+		) {
+			console.warn(`Time exceeded - ${label} - ${ToDigits(event.durationMS)}ms (${ESTTodayDateTimeLabel()})`)
+		}
+
+		return event
+	}
+
+	/**
+	 * Mark the current time with a label for analysis later, and throw a console warn if appropriate
+	 * @param label
+	 * @param promiseFunction
+	 * @param options
+	 */
+	public markResolved(label: string, promiseFunction: Promise<any>, options?: TTimeTrackerOptions) {
+		promiseFunction.then(() => {
+			this.mark(label, options)
+		})
+
+		return promiseFunction
+	}
+
+	/**
+	 * Report back only the events that exceeded the offending MS
+	 */
+	public get offendingEvents() {
+		return this.events.reduce<TTimeEvent[]>((results, event) => {
+			if (event.durationMS > this.offendingMS) {
+				return [...results, event]
+			}
+
+			return results
+		}, [])
+	}
+
+	/**
+	 * Analyze events, and if any are offending, or if the sum offends, then display those events and return them
+	 * @param options
+	 */
+	public durationOffends(options?: TTimeTrackerOptions & {label?: string; showAll?: boolean}) {
+		const offendingEvents = this.offendingEvents
+
+		if (offendingEvents.length || this.duration > (options?.offendingMS ?? this.offendingMS)) {
+			const useEvents = options?.showAll ? this.events.filter((event) => event.durationMS) : offendingEvents
+
+			if (options?.warnAutomatically ?? this.warnAutomatically) {
+				console.warn(`"${options?.label ?? 'Unknown Event'}" took ${ToDigits(this.duration)}ms`)
+				console.table(useEvents.map((oe) => OmitProperty(oe, 'eventMS')))
+			}
+			return useEvents
+		}
+
+		return null
+	}
+}
