4f3b347262625ad6ccf6a7b628b82878f98a098e
Dennis Peters <denjpeters@gmail.com>
2023-05-24 15:55:55 -0500

    Fix nullable array

--- consoles/consoles.ts
+++ consoles/consoles.ts
@@ -14,22 +14,6 @@ console.log(
 			name: null,
 			start_date: '1/1/2023',
 			ids: ['1', 2, 0, null, ''],
-			ids_null: [],
-			salary: '15000',
-			is_active: 'false'
-		} as any,
-		ObjectConstraintTest
-	)
-)
-
-console.log(
-	ConstrainObject(
-		{
-			id: '1',
-			name: null,
-			start_date: '1/1/2023',
-			ids: ['1', 2, 0, null, ''],
-			ids_null: null,
 			salary: '15000',
 			is_active: 'false'
 		} as any,
