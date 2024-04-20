551f458454a9ef8138de0ba429e70e3480efc5dc
Dennis Peters <denjpeters@gmail.com>
2024-04-20 13:25:04 -0400

    Update TypeScript typings and improve function documentation
    This commit improves the documentation for several functions in the UnselectedIDList file, especially function parameter and return types. It also introduces the TNumberObject type for clear function input specification. The overarching alteration is it refines code readability and understanding for future code maintenance and debugging activities.

--- src/UnselectedIDList.ts
+++ src/UnselectedIDList.ts
@@ -1,39 +1,41 @@
 /**
+ * The TNumberObject type represents a value that can either be a number or an object.
+ * This type is used when we want to define a variable that can have either a number or an object as its value.
  *
- *
+ * @typedef {number|object} TNumberObject
  */
 export type TNumberObject = number | object
 
 /**
+ * Function to retrieve the ID from an item.
  *
- * @param item
- * @constructor
- *
+ * @param {TNumberObject} item - The item from which to retrieve the ID.
+ * @returns {number} - The ID of the item.
  */
 export const ToID = (item: TNumberObject): number => typeof item === 'number' ? item : (item as any).id as number
 
 /**
- *
- *
+ * A namespace for managing a list of unselected IDs.
  */
 export namespace UnselectedIDList {
 
 	/**
+	 * Checks if the provided item is selected based on the unselected IDs list.
 	 *
-	 * @param item
-	 * @param unselectedIDs
-	 * @constructor
-	 *
- */
+	 * @param {TNumberObject} item - The item to check if it is selected.
+	 * @param {number[]} unselectedIDs - The list of unselected IDs.
+	 * @returns {boolean} - Returns `true` if the item is selected, `false` otherwise.
+	 */
 	export const IsSelected = (item: TNumberObject, unselectedIDs: number[]): boolean => !unselectedIDs.includes(ToID(item))
 
 	/**
+	 * This function filters out selected IDs from a given list of items based on a list of unselected IDs.
 	 *
-	 * @param items
-	 * @param unselectedIDs
-	 * @constructor
+	 * @param {TNumberObject[]} items - The list of items containing the IDs.
+	 * @param {number[]} unselectedIDs - The list of IDs to be excluded from the selection.
 	 *
- */
+	 * @returns {number[]} - The list of selected IDs after filtering.
+	 */
 	export const SelectedIDs = (items: TNumberObject[], unselectedIDs: number[]): number[] => items.reduce<number[]>(
 		(result, cur) => {
 			const curID: number = ToID(cur)
@@ -44,43 +46,43 @@ export namespace UnselectedIDList {
 	)
 
 	/**
+	 * Toggles the presence of a given ID in an array of unselected IDs.
 	 *
-	 * @param toggleID
-	 * @param unselectedIDs
-	 * @constructor
-	 *
- */
+	 * @param {number} toggleID - The ID to toggle.
+	 * @param {number[]} unselectedIDs - The array of unselected IDs.
+	 * @returns {number[]} - The updated array of unselected IDs.
+	 */
 	export const ToggleUnSelectedID = (toggleID: number, unselectedIDs: number[]): number[] => unselectedIDs.includes(toggleID) ? unselectedIDs.filter(id => id !== toggleID) : [...unselectedIDs, toggleID]
 
 	/**
+	 * Returns an array of selected IDs from the given list of IDs and unselected IDs.
 	 *
-	 * @param ids
-	 * @param unselectedIDs
-	 * @constructor
-	 *
- */
+	 * @param {TNumberObject[]} ids - The list of selected IDs.
+	 * @param {number[]} unselectedIDs - The list of unselected IDs.
+	 * @returns {number[]} - The array of selected IDs.
+	 */
 	export const SelectIDs = (ids: TNumberObject[], unselectedIDs: number[]): number[] =>
 		unselectedIDs.filter(unselectedID => !ids.find(id => unselectedID === ToID(id)))
 
 	/**
+	 * Adds the unselected IDs to the given array of IDs.
 	 *
-	 * @param ids
-	 * @param unselectedIDs
-	 * @constructor
-	 *
- */
+	 * @param {TNumberObject[]} ids - The array of IDs to add unselected IDs to.
+	 * @param {number[]} unselectedIDs - The array of unselected IDs to add.
+	 * @returns {number[]} - The updated array of IDs with unselected IDs added.
+	 */
 	export const UnSelectIDs = (ids: TNumberObject[], unselectedIDs: number[]): number[] =>
 		[...unselectedIDs, ...(ids.map(id => ToID(id)))]
 
 	/**
+	 * Filters an array of IDs and returns the IDs that are between the given lastID and nextID.
 	 *
-	 * @param allIDs
-	 * @param lastID
-	 * @param nextID
-	 * @param unselectedIDs
-	 * @constructor
-	 *
- */
+	 * @param {TNumberObject[]} allIDs - An array of TNumberObject containing all the IDs.
+	 * @param {number} lastID - The last ID in the sequence.
+	 * @param {number} nextID - The next ID in the sequence.
+	 * @param {number[]} unselectedIDs - An array of numbers representing the unselected IDs.
+	 * @returns {number[]} The modified array of numbers.
+	 */
 	export const SelectedBetween = (allIDs: TNumberObject[], lastID: number, nextID: number, unselectedIDs: number[]): number[] => {
 		const allNumbers: number[] = allIDs.map(allID => ToID(allID))
 
@@ -106,16 +108,17 @@ export namespace UnselectedIDList {
 }
 
 /**
+ * Filters an array of IDs and returns the IDs that are between the given lastID and nextID.
  *
- * @param allIDs
- * @param lastID
- * @param nextID
- * @param inclusive
- * @constructor
- *
+ * @template T - Type of IDs (number or string).
+ * @param {T[]} allIDs - Array of all IDs.
+ * @param {T} lastID - The ID that comes before the desired range.
+ * @param {T} nextID - The ID that comes after the desired range.
+ * @param {boolean} [inclusive=true] - Indicates whether the lastID and nextID should be included in the result.
+ * @returns {T[]} - Array of IDs between the lastID and nextID.
  */
-export const SelectBetweenIDs = (allIDs: number[], lastID: number, nextID: number, inclusive: boolean = true): number[] => {
-	let betweenIDs: number[] = []
+export const SelectBetweenIDs = <T extends number | string>(allIDs: T[], lastID: T, nextID: T, inclusive: boolean = true): T[] => {
+	let betweenIDs: T[] = []
 	let firstFound = false
 
 	for (const checkID of allIDs) {
